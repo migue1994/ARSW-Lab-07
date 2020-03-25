69ed565d2804c49639cfbc84ed03ba42781cd074
nduran06 <natalia.duran@mail.escuelaing.edu.co>
2020-03-25 00:06:05 -0500

    Punto 2

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,23 +1,23 @@
 var app = (function () {
 
-    class Point{
-        constructor(x,y){
-            this.x=x;
-            this.y=y;
-        }        
+    class Point {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+        }
     }
-    
+
     var stompClient = null;
 
-    var addPointToCanvas = function (point) {        
+    var addPointToCanvas = function (point) {
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
         ctx.beginPath();
         ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
         ctx.stroke();
     };
-    
-    
+
+
     var getMousePosition = function (evt) {
         canvas = document.getElementById("canvas");
         var rect = canvas.getBoundingClientRect();
@@ -28,39 +28,45 @@ var app = (function () {
     };
 
 
-    var connectAndSubscribe = function () {
+    var connectAndSubscribe = function (callback) {
         console.info('Connecting to WS...');
         var socket = new SockJS('/stompendpoint');
         stompClient = Stomp.over(socket);
-        
+
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
 
             //stompClient.subscribe('/topic/TOPICXX', function (eventbody) {
             stompClient.subscribe('/topic/newpoint', function (eventbody) {
-                var theObject=JSON.parse(eventbody.body);
-                
-                
+                var theObject = JSON.parse(eventbody.body);
+                callback(new Point(theObject.x, theObject.y));
+
             });
         });
 
     };
-    
-    
+
+
 
     return {
 
         init: function () {
             var can = document.getElementById("canvas");
-            
+
+            can.addEventListener("pointerdown", function(event){
+                var clickPos=getMousePosition(event);
+                var x=clickPos.x; var y=clickPos.y;
+                app.publishPoint(x, y);
+            })
+
             //websocket connection
-            connectAndSubscribe();
+            connectAndSubscribe(addPointToCanvas);
         },
 
-        publishPoint: function(px,py){
-            var pt=new Point(px,py);
-            console.info("publishing point at "+pt);
+        publishPoint: function (px, py) {
+            var pt = new Point(px, py);
+            console.info("publishing point at " + pt);
             addPointToCanvas(pt);
 
             //publicar el evento
